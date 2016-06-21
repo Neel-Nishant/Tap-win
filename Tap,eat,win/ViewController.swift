5b5819587fe39d8d174aa98852486d944edc46b3
Neel Nishant <Neel@Neels-MacBook-Air.local>
2016-06-21 20:08:24 +0530

    tap&win app

--- Tap,eat,win/ViewController.swift
+++ Tap,eat,win/ViewController.swift
@@ -9,16 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //variables
+    var maxtaps=0
+    var curtaps=0
+    
+    //outlets
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var taps: UITextField!
+    @IBOutlet weak var playbtn:UIButton!
+    
+    @IBOutlet weak var tapbtn: UIButton!
+    @IBOutlet weak var taplbl: UILabel!
+    
+    @IBAction func onPressingCoin(sender: UIButton!){
+        curtaps += 1
+        updatetaplbl()
+        if isGameOver(){
+            restartGame()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onPressingPlay(sender: UIButton!){
+       
+        
+        if (taps.text != nil && taps.text != "") {
+            logoImg.hidden=true
+            playbtn.hidden=true
+            taps.hidden=true
+            
+            tapbtn.hidden=false
+            taplbl.hidden=false
+            
+            maxtaps = Int(taps.text!)!
+            curtaps=0
+            
+            
+            updatetaplbl()
+        }
+        
     }
+    func restartGame(){
+        maxtaps=0
+        taps.text=""
+        logoImg.hidden=false
+        playbtn.hidden=false
+        taps.hidden=false
+        
+        tapbtn.hidden=true
+        taplbl.hidden=true
+        
+    }
+    func isGameOver() -> Bool{
+        if curtaps >= maxtaps
+        {return true}
+        else
+        {return false}
+    }
+    func updatetaplbl(){
+        taplbl.text = "\(curtaps) Taps"
+    }
+    
 
 
 }
